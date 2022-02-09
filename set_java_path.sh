3ff4cb920a9cbc4531fd874116b2350afcf937d5
Rutuja Walke <68474231+Rutuja-Walke@users.noreply.github.com>
2022-02-09 14:14:52 +0530

    added java binaries to path script

---
+++ set_java_path.sh
@@ -0,0 +1,6 @@
+$ echo $PATH
+/usr/local/bin:/bin:/usr/bin:/usr/sun/jdk/v1.6.0_16-64bit/bin:.
+
+vim ~/bash_profile export JAVA_HOME=/usr/sun/jdk/v1.6.0_16-64bit/ export PATH = $JAVA_HOME/bin:$PATH
+
+Read more: https://www.java67.com/2016/06/how-to-set-javahome-and-path-in-linux.html#ixzz7KNvH4K3g
