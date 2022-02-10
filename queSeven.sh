1253f16612f140d60cda5aae50c481af5dd8b31c
Rutuja Walke <68474231+Rutuja-Walke@users.noreply.github.com>
2022-02-10 10:02:25 +0530

    updated time stamp script

--- queSeven.sh
+++ queSeven.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-DIRectory="dirone"  #write directory path here
+DIRECTORY="dirone"  #write directory path here
 
 if [[ -d "$DIRECTORY" ]]
 then
     echo "$DIRECTORY exists on your filesystem."
-    #update the timestamps of files inside the directory
+    #update the timestamps of files inside the directory 
+    file_name=test_files.txt
+ 
+    current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+    echo "Current Time : $current_time"
+ 
+    new_fileName=$file_name.$current_time
+    echo "New FileName: " "$new_fileName"
+ 
+    cp $file_name $new_fileName
+    echo "Timestamp updated"
+
 else
     mkdir "$DIRECTORY"
     cd "$DIRECTORY"
     echo 'Hello World' > test.txt
+    echo "Printing test file data"
+    cat test.txt
 fi
-
-echo "Printing test file data"
-cat test.txt
